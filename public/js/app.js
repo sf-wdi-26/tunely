313258afd431bba514f1c09cf1055616de40c733
tgaff <tgaff@alumni.nd.edu>
2015-12-11 18:30:21 -0800

    added handlebars for one album at a time

--- public/js/app.js
+++ public/js/app.js
@@ -38,6 +38,9 @@ sampleAlbums.push({
 
 
 $(document).ready(function() {
+  sampleAlbums.forEach(function(album) {
+    renderAlbum(album);
+  });
   console.log('app.js loaded!');
 });
 
@@ -46,47 +49,15 @@ $(document).ready(function() {
 
 
 // this function takes a single album and renders it to the page
-function renderAlbum(album) {
-  console.log('rendering album:', album);
+function renderAlbum(taco) {
+  console.log('rendering album:', taco);
+  var template = $('#albumTemplate').html();
+  console.log('template', template);
 
-  var albumHtml =
-  "        <!-- one album -->" +
-  "        <div class='row album' data-album-id='" + "HARDCODED ALBUM ID" + "'>" +
-  "          <div class='col-md-10 col-md-offset-1'>" +
-  "            <div class='panel panel-default'>" +
-  "              <div class='panel-body'>" +
-  "              <!-- begin album internal row -->" +
-  "                <div class='row'>" +
-  "                  <div class='col-md-3 col-xs-12 thumbnail album-art'>" +
-  "                     <img src='" + "http://placehold.it/400x400'" +  " alt='album image'>" +
-  "                  </div>" +
-  "                  <div class='col-md-9 col-xs-12'>" +
-  "                    <ul class='list-group'>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Album Name:</h4>" +
-  "                        <span class='album-name'>" + "HARDCODED ALBUM NAME" + "</span>" +
-  "                      </li>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Artist Name:</h4>" +
-  "                        <span class='artist-name'>" + "HARDCODED ARTIST NAME" + "</span>" +
-  "                      </li>" +
-  "                      <li class='list-group-item'>" +
-  "                        <h4 class='inline-header'>Released date:</h4>" +
-  "                        <span class='album-releaseDate'>" + "HARDCODED RELEASE DATE" + "</span>" +
-  "                      </li>" +
-  "                    </ul>" +
-  "                  </div>" +
-  "                </div>" +
-  "                <!-- end of album internal row -->" +
+  // compiledTemplate is actually a function!
+  var compiledTemplate = Handlebars.compile(template);
+  var htmlFromCompiledTemplate = compiledTemplate( { album: taco } );
 
-  "              </div>" + // end of panel-body
+  $('#albums').prepend( htmlFromCompiledTemplate );
 
-  "              <div class='panel-footer'>" +
-  "              </div>" +
-
-  "            </div>" +
-  "          </div>" +
-  "          <!-- end one album -->";
-
-  // render to the page with jQuery
 }
