71bf6adc8c497c943b5ea9aa4ab746849cd91994
tgaff <tgaff@alumni.nd.edu>
2015-12-14 10:07:15 -0800

    Add _id with handlebars

--- public/js/app.js
+++ public/js/app.js
@@ -9,24 +9,28 @@
 /* hard-coded data! */
 var sampleAlbums = [];
 sampleAlbums.push({
+             _id: '12',
              artistName: 'Ladyhawke',
              name: 'Ladyhawke',
              releaseDate: '2008, November 18',
              genres: [ 'new wave', 'indie rock', 'synth pop' ]
            });
 sampleAlbums.push({
+             _id: '13',
              artistName: 'The Knife',
              name: 'Silent Shout',
              releaseDate: '2006, February 17',
              genres: [ 'synth pop', 'electronica', 'experimental' ]
            });
 sampleAlbums.push({
+             _id: '14',
              artistName: 'Juno Reactor',
              name: 'Shango',
              releaseDate: '2000, October 9',
              genres: [ 'electronic', 'goa trance', 'tribal house' ]
            });
 sampleAlbums.push({
+             _id: '15',
              artistName: 'Philip Wesley',
              name: 'Dark Night of the Soul',
              releaseDate: '2008, September 12',
