d040ddd9c2bd5f01936c54ccbc2bdc78b394b519
JDe-Lora <jdelora17@gmail.com>
2016-03-09 08:51:15 -0800

    addedhww08d02

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,33 @@
  */
 
 
+var express        = require('express');
+var path           = require('path');
+var logger         = require('morgan');
+var bodyParser     = require('body-parser');
+var app            = express();
+var mongoose       = require('mongoose');
+var passport       = require('passport');
+var expressSession = require('express-session');
+var cookieParser   = require("cookie-parser");
+
+
+// Middleware
+app.use( cookieParser() );
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// Setting up the Passport Strategies
+require("./config/passport")(passport);
+
+
 
 $(document).ready(function() {
   console.log('app.js loaded!');
